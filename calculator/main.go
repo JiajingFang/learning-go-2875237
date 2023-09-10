7f7f0569fae593c238bbcff878d88c3f22c55b64
JiajingFang <54315631+JiajingFang@users.noreply.github.com>
2023-09-10 16:31:44 +0200

    new

--- calculator/main.go
+++ calculator/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("A simple calculator")
+
+	fmt.Print("Value 1: ")
+	reader := bufio.NewReader(os.Stdin)
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		panic("value 1 must be a number")
+	}
+
+	fmt.Print("Value 2: ")
+	reader = bufio.NewReader(os.Stdin)
+	numInput, _ = reader.ReadString('\n')
+	bFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		panic("value 2 must be a number")
+	}
+
+	fmt.Println("the sum of ", aFloat, " and ", bFloat, " is ", math.Round((aFloat+bFloat)*100)/100)
 }
