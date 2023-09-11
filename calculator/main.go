9b5509ae959352aa28b815407bcb4e1393bcb6aa
JiajingFang <krisssfong@gmail.com>
2023-09-11 16:24:12 +0200

    new

--- calculator/main.go
+++ calculator/main.go
@@ -14,22 +14,62 @@ func main() {
 
 	fmt.Print("Value 1: ")
 	input1, _ := reader.ReadString('\n')
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
-	}
+	float1 := valiValue(input1)
 
 	fmt.Print("Value 2: ")
 	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
+	float2 := valiValue(input2)
+
+	fmt.Print("Select your operation +-*/: ")
+	input3, _ := reader.ReadString('\n')
+	input3 = strings.TrimSpace(input3)
+
+	switch input3 {
+	case "+":
+		add(float1, float2)
+
+	case "-":
+		minus(float1, float2)
+
+	case "*":
+		multi(float1, float2)
+
+	case "/":
+		division(float1, float2)
+
 	}
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+}
 
+func valiValue(input string) float64 {
+	float1, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value must be a number")
+	}
+	return float1
+}
+
+func add(value1, value2 float64) {
+	sum := value1 + value2
+	sum = math.Round(sum*100) / 100
+	fmt.Printf("The sum of %v and %v is %v\n\n", value1, value2, sum)
+}
+
+func minus(value1, value2 float64) {
+	sum := value1 - value2
+	sum = math.Round(sum*100) / 100
+	fmt.Printf("The minus of %v and %v is %v\n\n", value1, value2, sum)
+}
+
+func multi(value1, value2 float64) {
+	sum := value1 * value2
+	sum = math.Round(sum*100) / 100
+	fmt.Printf("The multiplication of %v and %v is %v\n\n", value1, value2, sum)
+}
+
+func division(value1, value2 float64) {
+	sum := value1 / value2
+	sum = math.Round(sum*100) / 100
+	fmt.Printf("The division of %v and %v is %v\n\n", value1, value2, sum)
 }
